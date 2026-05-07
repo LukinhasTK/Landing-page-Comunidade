c34f0b923105c374448b69b14ea6e56236ae8561
LukinhasTK <lucas.tkn14@gmail.com>
2026-05-07 15:13:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,33 @@ function changeImage() {
 
 setInterval(changeImage, 3000)
 
+const slides = document.querySelectorAll('.slider-box')
+const nextButton = document.getElementById('next')
+const prevButton = document.getElementById('prev')
+const dots = document.querySelectorAll('.nav-btn-links')
+
+let slideActive = 0
+
+function updateDots() {
+  dots.forEach(dot => dot.classList.remove('active'))
+  dots[slideActive].classList.add('active')
+}
+
+
+nextButton.addEventListener('click', () => {
+  slides[slideActive].classList.remove('active')
+  slideActive = (slideActive + 1) % slides.length
+  slides[slideActive].classList.add('active')
+   updateDots()
+})
+
+prevButton.addEventListener('click', () => {
+  slides[slideActive].classList.remove('active')
+  slideActive = (slideActive - 1 + slides.length) % slides.length
+  slides[slideActive].classList.add('active')
+   updateDots()
+})
+
 $(document).ready(function() {
   $('.mobile-btn').on('click', function (){ 
     $('.mobile-menu').toggleClass('active')
